e84cb7daeee2d28abdf0691fbeaa2e04a3ea4286
Dawid Tomaszewski <dawidtomaszewski672@gmail.com>
2024-10-25 15:42:03 +0200

    Dodaj pliki projektów.

---
+++ GroupTaskManager/GroupTaskManager.Database/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace GroupTaskManager.GroupTaskManager.Database
+{
+    public class DatabaseContext : IdentityDbContext<UserModel>
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
